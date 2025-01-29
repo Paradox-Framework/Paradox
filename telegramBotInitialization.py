601fab6c76e3d9c85719a9d8dfeeecc6f988d77b
Paradox-Framework <cflaicoin@gmail.com>
2025-01-29 12:37:25 -0700

    Update telegramBotInitialization.py

--- telegramBotInitialization.py
+++ telegramBotInitialization.py
@@ -1,10 +1,8 @@
 from telegram.ext import Updater
 
 def start_telegram_bot(agent):
-    # Start Telegram bot with API key
     updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
     
-    # Load commands into bot
     bot_handler = ParadoxTelegramBot(TELEGRAM_BOT_TOKEN, agent, int(TELEGRAM_ADMIN_CHAT_ID))
     
     bot_handler.run()
