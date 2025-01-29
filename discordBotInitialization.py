487e390abdb8dae7a24a9ac829ca52f508df3020
Paradox-Framework <cflaicoin@gmail.com>
2025-01-29 12:38:15 -0700

    Update discordBotInitialization.py

--- discordBotInitialization.py
+++ discordBotInitialization.py
@@ -3,13 +3,10 @@ from discord.ext import commands
 def start_discord_bot(agent):
     bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
     
-    # Load Discord bot with API key
     @bot.event
     async def on_ready():
         print(f"Logged in as {bot.user}")
 
-    # Load commands into bot
     bot.add_cog(ParadoxDiscordBot(bot, agent, int(DISCORD_ADMIN_CHANNEL_ID)))
 
-    # Start bot
     bot.run(DISCORD_BOT_TOKEN)
